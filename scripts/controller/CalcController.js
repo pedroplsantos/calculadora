4745d0583ef4a2b9455cdf18cb822c6dd75dacbf
Pedro Lima <pplsantos89@gmail.com>
2020-06-15 23:04:33 -0300

    Evento
    Adicionando event click

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -6,6 +6,7 @@ class CalcController{
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
 
     }
 
@@ -19,6 +20,20 @@ class CalcController{
 
         },1000);
     }
+
+    initButtonsEvents(){
+
+        let buttons = document.querySelectorAll("#buttons >g, #parts > g");
+
+        buttons.forEach((btn, index)=>{
+            btn.addEventListener("click", e => {
+                console.log(btn.className.baseVal.replace("btn-",""))
+            });
+        })
+
+    }
+
+
 
     setDisplayDateTime(){
         this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
