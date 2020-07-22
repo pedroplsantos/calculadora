19858a611856932a0b113d7aa238aefaf7e69346
Pedro Lima <pplsantos89@gmail.com>
2020-07-22 18:26:59 -0300

    switch

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -29,12 +29,72 @@ class CalcController{
         })
 
     }
-    execBtn(){
+
+    clearAll(){
+
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+
+    }
+
+    addOperation(value){
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value){
 
         switch(value){
             case "ac":
+                this.clearAll();
+                break;
 
+            case "ce":
+                this.clearEntry();
+                break;
+
+            case "soma":
+                break;
+
+            case "subtracao":
+                break;
+
+            case "divisao":
+                break;
+
+            case "multiplicacao":
+                break;
+
+            case "porcento":
+                break;
+
+            case "igual":
             break;
+
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError     
+                break;
         } 
     };
 
